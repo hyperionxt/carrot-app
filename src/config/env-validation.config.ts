4cd53fb15fe5ae0e8b411c43ce6889a8295b0c6e
raijel <ngcthonym42@gmail.com>
2024-06-14 17:57:33 -0400

    deploy config

--- src/config/env-validation.config.ts
+++ src/config/env-validation.config.ts
@@ -1,14 +1,14 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  APP_PORT: z.string(),
+  //APP_PORT: z.string(),
   DB_PORT: z.string(),
   DB_TYPE: z.string(),
   DB_HOST: z.string(),
   DB_USERNAME: z.string(),
   DB_PASSWORD: z.string(),
   DB_NAME: z.string(),
-  DB_NAME_TEST: z.string(),
+  //DB_NAME_TEST: z.string(),
   JWT_SECRET_KEY: z.string(),
   //DUMP_PATH: z.string(),
   //BACKUP_PATH: z.string(),
